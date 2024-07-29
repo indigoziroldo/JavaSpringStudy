89fc3a95f5d5b4c0cb3a9308d4498d995ea58ea8
indigo <indigoziroldo@gmail.com>
2024-07-29 14:20:26 -0300

    feat: student entity with jpa relationship with adress and course

--- src/main/java/com/indigo/spring_hydra/api/domain/entity/StudentEntity.java
+++ src/main/java/com/indigo/spring_hydra/api/domain/entity/StudentEntity.java
@@ -1,17 +1,26 @@
 package com.indigo.spring_hydra.api.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
+@Table(name = "STUDENT")
 public class StudentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
+    private String name;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "adress_id", referencedColumnName = "id")
+    private AdressEntity adress;
+
+    @ManyToMany
+    @JoinTable(     name = "student_course",
+                    joinColumns = @JoinColumn(name = "student_id"),
+                    inverseJoinColumns = @JoinColumn(name = "course_id"))
+    Set<CourseEntity> courses;
 }
