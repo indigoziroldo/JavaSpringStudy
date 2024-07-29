a7dff7932c42e48d923011cbc4a6c71f87406759
Indigo Ziroldo <126610921+indigoziroldo@users.noreply.github.com>
2024-07-29 12:18:00 -0300

    Initial commit

---
+++ src/main/java/com/indigo/spring_hydra/SpringHydraApplication.java
@@ -0,0 +1,13 @@
+package com.indigo.spring_hydra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHydraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHydraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indigo/spring_hydra/SpringHydraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indigo.spring_hydra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHydraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
