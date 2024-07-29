fb77a99d6ac99b03832c9511cf3ae09ea90cb95a
indigo <indigoziroldo@gmail.com>
2024-07-29 14:31:29 -0300

    feat: adress entity with embedded city and one to one relationship with student

---
+++ src/main/java/com/indigo/spring_hydra/api/domain/entity/AdressEntity.java
@@ -0,0 +1,19 @@
+package com.indigo.spring_hydra.api.domain.entity;
+
+import com.indigo.spring_hydra.api.domain.entity.embeddable.CityEmbeddable;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ADRESS")
+public class AdressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private CityEmbeddable city;
+
+    @OneToOne(mappedBy = "adress")
+    private StudentEntity student;
+}
